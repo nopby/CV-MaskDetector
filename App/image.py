b69b4377be36a6513a10533724080b0f9c1ddada
nopby <byronharnope@gmail.com>
2021-12-22 06:03:01 +0700

    update files

--- App/image.py
+++ App/image.py
@@ -21,24 +21,48 @@ class Img:
         self.button.pack(expand=True, fill="both")
 
     def show(self, image):
+        # Muat gambar
         self.image = cv2.imread(image)
+
+        # Resize gambar
         self.image = cv2.resize(self.image, (800, 600), interpolation=cv2.INTER_AREA)
+
+        # Predict gambar
         (locs, preds) = self.controller.utils.pred(self.image)
+
+        # Temukan bounding box dan hasil prediksi
         for (box, pred) in zip(locs, preds):
+            # Simpan koordinat bounding box
             (x0, y0, x1, y1) = box
+            # Simpan hasil prediksi
             (mask, withoutMask) = pred
 
+            # Berikan label untuk setiap prediksi
             label = "Mask" if mask > withoutMask else "No Mask"
+            
+            # Berikan warna untuk setiap prediksi
             color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
 
+            # Tambahkan persentasi prediksi pada label
             label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
+            # Buat label pada gambar
             cv2.putText(self.image, label, (x0, y0 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+
+            # Buat bounding box pada gambar
             cv2.rectangle(self.image, (x0, y0), (x1, y1), color, 2)
 
+
+        # Ubah warna gambar ke RGB
         self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
+
+        # Ubah format gambar ke PIL Image untuk simpan file gambar
         self.image = Image.fromarray(self.image)
+
+        # Ubah format PIL Image ke ImageTk untuk ditampilkan
         imagetk = ImageTk.PhotoImage(image=self.image)
+
+        # Tampilkan ImageTk
         self.label.imagetk = imagetk
         self.label.configure(image=self.label.imagetk)
 
--- App/utils.py
+++ App/utils.py
@@ -9,42 +9,77 @@ from .constant import MODEL, PROTOTEXT, NET
 
 class Utils:
     def __init__(self):
+        # Load model face detection
         self.faceNet = cv2.dnn.readNet(PROTOTEXT, NET)
+
+        # Load model mask detection
         self.maskNet = load_model(MODEL)
 
 
-    def pred(self, frame):
+    def pred(self, image):
         
-        (h, w) = frame.shape[:2]
-        blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224), (104.0, 177.0, 123.0))
+        # Dapatkan ukuran gambar
+        (h, w) = image.shape[:2]
+
+        # Dapatkan blob pada gambar
+        blob = cv2.dnn.blobFromImage(image, 1.0, (224, 224), (104.0, 177.0, 123.0))
         
+        # Masukkan semua koleksi blob ke dalam network
         self.faceNet.setInput(blob)
+
+        # Lakukan forward untuk menghitung output layer
         detections = self.faceNet.forward()
         
+        # Siapkan penyimpanan untuk wajah, lokasi, dan prediksi
         faces = []
         locs = []
         preds = []
+
+        # Cari wajah
         for i in range(0, detections.shape[2]):
+
+            # Dapatkan nilai confidence
             confidence = detections[0, 0, i, 2]
 
             if confidence > 0.5:
+                # Dapatkan lokasi bounding box 
                 box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+
+                # Ubah tipe data bounding box ke integer
                 (x0, y0, x1, y1) = box.astype("int")
 
+
+                # Dapatkan koordinat bounding box
                 (x0, y0) = (max(0, x0), max(0, y0))
                 (x1, y1) = (min(w-1, x1), min(h-1, y1))
 
-                face = frame[y0:y1, x0:x1]
+
+                # Dapatkan lokasi bounding box pada gambar wajah
+                face = image[y0:y1, x0:x1]
+
+                # Ubah warna gambar ke RGB
                 face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+
+                # Resize gambar wajah sesuai dengan model deteksi masker
                 face = cv2.resize(face, (224, 224))
+
+                # Ubah format ke numpy array
                 face = img_to_array(face)
+
+                # Sesuaikan nilai dengan model caffe
                 face = preprocess_input(face)
 
+                # Simpan gambar dan lokasi bounding box
                 faces.append(face)
                 locs.append((x0, y0, x1, y1))
 
+        # Cek jika wajah ada
         if len(faces) > 0:
+            # Ubah gambar wajah ke numpy array float
             faces = np.array(faces, dtype="float32")
+
+            # Prediksi masker pada wajah
             preds = self.maskNet.predict(faces, batch_size=32)
 
+        # Dapatkan lokasi wajah dan hasil prediksi masker
         return (locs, preds)
